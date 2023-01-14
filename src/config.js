3ac7fec61bb9c2c36a6d9b379642654566cf31f5
prem-jethwa <premjethwa788@gmail.com>
2023-01-14 14:33:32 +0530

    API_URL update

--- src/config.js
+++ src/config.js
@@ -10,5 +10,5 @@ export const NO_SONG_OBJ = {
 
 export const AUTO_LOGOUT = true;
 export const PER_PAGE = 3;
-export const API_URL = "https://koel-api.herokuapp.com/"; //PROD
+export const API_URL = "https://koel-api-main.vercel.app/"; //PROD
 // export const API_URL = "http://localhost:3000/"; //LOCAL
