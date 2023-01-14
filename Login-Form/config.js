a0f81e5999b3e6ae8fa62ef9861bf15ffd384e4f
prem-jethwa <premjethwa788@gmail.com>
2023-01-14 17:57:31 +0530

    API url updated

--- Login-Form/config.js
+++ Login-Form/config.js
@@ -1,4 +1,4 @@
 export const HOME_URL = "../index.html";
 
-export const API_URL = "https://koel-api.herokuapp.com/"; //PROD
+export const API_URL = "https://koel-api-main.vercel.app/"; //PROD
 // export const API_URL = "http://localhost:3000/"; //LOCAL
